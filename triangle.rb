268f780a2e8dddae638c735a5c4834fea062c301
Alexander Khramov <alx.khramov@gmail.com>
2015-07-20 01:35:01 +0300

    solve about_triangle_project_2

--- triangle.rb
+++ triangle.rb
@@ -14,14 +14,24 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  if a == 0 || b == 0 || c == 0
+    raise TriangleError, "A triangle should not have any sides with zero length"
+  end
+  if a < 0 || b < 0 || c < 0
+   raise TriangleError, "A triangle should not have any sides with negative length"
+  end
+  if a + b <= c || a + c <= b || b + c <= a
+    raise TriangleError, "Any two sides of a triangle should add up to more than the third side"
+  end
+
   result =
-	  if a == b && a == c
-	  	:equilateral
-	  elsif a != b && a != c && b != c
-  		:scalene
-  	  else
-  	  	:isosceles
-  	  end
+    if a == b && a == c
+      :equilateral
+    elsif a != b && a != c && b != c
+      :scalene
+    else
+      :isosceles
+    end
 end
 
 # Error class used in part 2.  No need to change this code.
